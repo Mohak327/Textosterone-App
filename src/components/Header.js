01a7ad1d7f2c3b87a731d75d361477b5ff45596b
Mohak Sharma <mohak327@gmail.com>
2021-04-01 19:10:56 +0530

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,24 @@ import React from 'react'
 import "./Header.css"
 import PersonIcon from '@material-ui/icons/Person'
 import ForumIcon from '@material-ui/icons/Forum'
+// Only for ripple effect.
+import IconButton from '@material-ui/core/IconButton'
 
 function Header() {
     return (
         <div className="header">
-            <PersonIcon/>
+            <IconButton>
+                <PersonIcon className="header__icon" fontSize = "large"/>
+            </IconButton>
+
                 <img
                     className="header__logo"
                     src="https://cdn.worldvectorlogo.com/logos/tinder-2.svg"
                     alt="Textosterone"/>
-            <ForumIcon/>
+
+            <IconButton>
+                <ForumIcon className="header__icon" fontSize = "large"/>
+            </IconButton>
         </div>
     )
 }
